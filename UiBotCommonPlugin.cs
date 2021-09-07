c69286fe058be5e63e74025c72d5df2d4054813e
Sean <sean@everbiz.com.tw>
2021-09-07 19:11:03 +0800

    TextFromPage, ThumbnailImage

--- UiBotCommonPlugin.cs
+++ UiBotCommonPlugin.cs
@@ -1,5 +1,14 @@
-﻿using Newtonsoft.Json.Linq;
+﻿
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 
 //建议把下面的namespace名字改为您的插件名字
@@ -10,6 +19,13 @@ namespace UiBotCommonPlugin
   string GetFileCreationTime(string path);
   string GetFileLastWriteTime(string path);
   void SmtpSendHtmlMail(string Host, int Port, string Account, string Password, string Subject, string Body, string From, string To, string Cc, string Bcc);
+
+  void PdfExtractPages(string sourcePDFpath, string outputPDFpath, int startpage, int endpage);
+  void PdfToXls(string source, string xlspath);
+
+  string TextFromPage(string _filePath, int startPage, int endPage);
+
+  void ThumbnailImage(string filepath, int width, int height);
  }
 
  public class Plugin_Implement : Plugin_Interface
@@ -39,10 +55,10 @@ namespace UiBotCommonPlugin
     msg.From = new System.Net.Mail.MailAddress(From);
 
     foreach (Match item in new Regex(@"[\w-\.]+@([\w-]+\.)+[\w-]{2,4}", RegexOptions.IgnoreCase).Matches(To)) msg.To.Add(item.Value);
-    if(Cc != null)
-    foreach (Match item in new Regex(@"[\w-\.]+@([\w-]+\.)+[\w-]{2,4}", RegexOptions.IgnoreCase).Matches(Cc)) msg.CC.Add(item.Value);
-    if(Bcc != null)
-    foreach (Match item in new Regex(@"[\w-\.]+@([\w-]+\.)+[\w-]{2,4}", RegexOptions.IgnoreCase).Matches(Bcc)) msg.Bcc.Add(item.Value);
+    if (Cc != null)
+     foreach (Match item in new Regex(@"[\w-\.]+@([\w-]+\.)+[\w-]{2,4}", RegexOptions.IgnoreCase).Matches(Cc)) msg.CC.Add(item.Value);
+    if (Bcc != null)
+     foreach (Match item in new Regex(@"[\w-\.]+@([\w-]+\.)+[\w-]{2,4}", RegexOptions.IgnoreCase).Matches(Bcc)) msg.Bcc.Add(item.Value);
     msg.Subject = Subject;
     smtp.Send(msg);
    }
@@ -50,5 +66,188 @@ namespace UiBotCommonPlugin
    {
    }
   }
+  public void PdfExtractPages(string sourcePDFpath, string outputPDFpath, int startpage, int endpage)
+  {
+   PdfReader reader = null;
+   Document sourceDocument = null;
+   PdfCopy pdfCopyProvider = null;
+   PdfImportedPage importedPage = null;
+
+   reader = new PdfReader(sourcePDFpath);
+   sourceDocument = new Document(reader.GetPageSizeWithRotation(startpage));
+   pdfCopyProvider = new PdfCopy(sourceDocument, new System.IO.FileStream(outputPDFpath, System.IO.FileMode.Create));
+
+   sourceDocument.Open();
+
+   for (int i = startpage; i <= endpage; i++)
+   {
+    importedPage = pdfCopyProvider.GetImportedPage(reader, i);
+    pdfCopyProvider.AddPage(importedPage);
+   }
+   sourceDocument.Close();
+   reader.Close();
+  }
+  public void PdfToXls(string filename, string xlspath)
+  {
+   Spire.Pdf.PdfDocument doc = new Spire.Pdf.PdfDocument(filename);
+   doc.SaveToFile(xlspath, Spire.Pdf.FileFormat.XLSX);
+  }
+  public string TextFromPage(string _filePath, int startPage, int endPage)
+  {
+   var pdfReader = new PdfReader(_filePath);
+
+
+   var locationTextExtractionStrategy = new TextWithFontExtractionStategy();
+   string textFromPage = "";
+   for (int i = startPage; i <= (endPage != -1 ? endPage : startPage); i++)
+    textFromPage += PdfTextExtractor.GetTextFromPage(pdfReader, i, locationTextExtractionStrategy);
+
+   return Encoding.UTF8.GetString(Encoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(textFromPage)));
+
+
+  }
+
+
+  public void ThumbnailImage(string filepath, int width, int height)
+  {
+   MemoryStream stream = new MemoryStream(File.ReadAllBytes(filepath));
+   System.Drawing.Image img = System.Drawing.Image.FromStream(stream);
+   Bitmap img1 = new Bitmap(width, height);
+   Graphics g = Graphics.FromImage(img1);
+   g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+   g.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
+   g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+   g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+   g.DrawImage(img, new System.Drawing.Rectangle(0, 0, img1.Width, img1.Height), new System.Drawing.Rectangle(0, 0, img.Width, img.Height), GraphicsUnit.Pixel);
+   g.Dispose();
+   img.Dispose();
+   stream.Close();
+   img1.Save(filepath, filepath.EndsWith(".png", StringComparison.InvariantCultureIgnoreCase) ? ImageFormat.Png : ImageFormat.Jpeg);
+  }
+
+  public class TextWithFontExtractionStategy : iTextSharp.text.pdf.parser.ITextExtractionStrategy
+  {
+   //HTML buffer
+   private StringBuilder result = new StringBuilder();
+   // 用來存放文字的矩形
+   List<System.util.RectangleJ> rectText = new List<System.util.RectangleJ>();
+
+   // 用來存放文字
+   List<String> textList = new List<String>();
+
+   // 用來存放文字的Y座標
+   List<float> listY = new List<float>();
+   List<float> listX = new List<float>();
+
+   // 用來存放每一行文字的座標位置
+   List<Dictionary<String, System.util.RectangleJ>> row_text_rect = new List<Dictionary<String, System.util.RectangleJ>>();
+
+   Dictionary<String, String> text_position = new Dictionary<String, String>();
+   // 圖片座標
+   List<float[]> arrays = new List<float[]>();
+
+   // 圖片
+   List<byte[]> arraysByte = new List<byte[]>();
+
+   //http://api.itextpdf.com/itext/com/itextpdf/text/pdf/parser/TextRenderInfo.html
+   private enum TextRenderMode
+   {
+    FillText = 0,
+    StrokeText = 1,
+    FillThenStrokeText = 2,
+    Invisible = 3,
+    FillTextAndAddToPathForClipping = 4,
+    StrokeTextAndAddToPathForClipping = 5,
+    FillThenStrokeTextAndAddToPathForClipping = 6,
+    AddTextToPaddForClipping = 7
+   }
+
+
+
+   public void RenderText(iTextSharp.text.pdf.parser.TextRenderInfo renderInfo)
+   {
+    String text = renderInfo.GetText().Trim();
+
+    if (text.Length > 0)
+    {
+     System.util.RectangleJ rectBase = renderInfo.GetBaseline().GetBoundingRectange();
+     // 獲取文字下面的矩形
+     System.util.RectangleJ rectAscen = renderInfo.GetAscentLine().GetBoundingRectange();
+     // 計算出文字的邊框矩形
+     //float leftX = (float)rectBase.X;
+     //float leftY = (float)(rectBase.Y - 1);
+     //float rightX = (float)rectBase.Width;
+     //float rightY = (float)(rectBase.Height - 1);
+     //Rectangle r = rectBase.GetBounds();
+     //// System.out.println("float:" + leftX + ":" + leftY + ":" + rightX
+     //// + ":" + rightY);
+     //Rectangle rect = new Rectangle(rectBase.X, leftY, rightX - leftX, rightY - leftY);
+     //// System.out.println("text:" + text + "X:" + rect.x + "Y:" + rect.y
+     //// + "width:" + rect.width + "height:"
+     //// + rect.height);
+     if (listY.Contains(rectBase.Y))
+     {
+      int index = listY.IndexOf(rectBase.Y);
+      float tempx = rectBase.X > rectText[index].X ? rectText[index].X : rectBase.X;
+      rectText[index] = new System.util.RectangleJ(tempx, rectBase.Y, rectBase.Width + rectText[index].Width, rectBase.Height);
+      textList[index] = textList[index] + text;
+     }
+     else
+     {
+      rectText.Add(rectBase);
+      textList.Add(text);
+      listY.Add(rectBase.Y);
+     }
+     if (!listX.Contains(rectBase.X))
+     {
+      listX.Add(rectBase.X);
+     }
+     text_position[rectBase.X + "," + rectBase.Y] = text;
+
+
+     Dictionary<String, System.util.RectangleJ> map = new Dictionary<String, System.util.RectangleJ>();
+     map[text] = rectBase;
+     row_text_rect.Add(map);
+    }
+   }
+
+   public string GetResultantText()
+   {
+    string result = "";
+    listY.Sort();
+    listX.Sort();
+    for (int j = listY.Count - 1; j >= 0; j--)
+    {
+     string line = "";
+     bool first = true;
+     float lastLeft = 0;
+     for (int i = 0; i < listX.Count; i++)
+     {
+      if (text_position.ContainsKey(listX[i] + "," + listY[j]))
+      {
+       if (first)
+       {
+        for (int k = 0; k <= listX[i]; k += 20) line += " ";
+       }
+       else
+       {
+        for (float k = lastLeft; k <= listX[i] - 20; k += 20) line += " ";
+       }
+       line += text_position[listX[i] + "," + listY[j]];
+       first = false;
+       lastLeft = listX[i];
+      }
+     }
+     if (line != "") result += line + "\n";
+    }
+    return result;
+   }
+
+   //Not needed
+   public void BeginTextBlock() { }
+   public void EndTextBlock() { }
+   public void RenderImage(ImageRenderInfo renderInfo) { }
+  }
  }
+
 }
